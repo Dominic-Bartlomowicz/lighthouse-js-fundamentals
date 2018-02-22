94992bb8338b2df2be08301ea407c6844be75319
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-02-22 14:53:50 -0800

    Adds functions exercises

---
+++ lastIndexOf.js
@@ -0,0 +1,43 @@
+function lastIndexOf(array,value)
+
+{
+
+console.log("\n");
+
+var end = false;
+var result = 0;		
+
+for (var i = array.length; i >= 0; i--)
+	
+	{
+
+		if ((array[i]) == value)
+			
+			{
+				console.log(i);
+				end = true;
+			
+				break;
+			}
+	}		
+
+	
+		if (end == true)
+
+			{
+				return result;
+			} 
+		else 
+			{
+				console.log("-1");
+			}
+
+
+	}
+
+
+lastIndexOf([ 0, 1, 4, 1, 2 ], 1)
+lastIndexOf([ 0, 1, 4, 1, 2 ], 2)
+lastIndexOf([ 0, 1, 4, 1, 2 ], 3)
+lastIndexOf([ 5, 5, 5 ], 5)
+lastIndexOf([], 3)
